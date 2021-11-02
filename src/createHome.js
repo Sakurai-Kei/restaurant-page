d5b498439a407b5e2c54167392f62adf0011fd05
R_Ashfell <luqmanulraiserhakim@gmail.com>
2021-11-03 05:59:02 +0800

    Placeholder replacement for Home is menu. Menu is 4/6.

--- src/createHome.js
+++ src/createHome.js
@@ -1,28 +1,36 @@
-// import homePicture1 from './picture1.png';
-// import homePicture2 from './picture2.png';
-// import homePicture3 from './picture3.png';
+import homePicture1 from './homePicture1.png';
+import homePicture2 from './homePicture2.png';
+import homePicture3 from './homePicture3.png';
 
 function createHome() {
-    let homePictureCollection = ['homePicture1', 'homePicture2', 'homePicture3'];
+    let homePictureCollection = [homePicture1, homePicture2, homePicture3];
+    const homeDescription1 = "We use the best\
+    ingredient fresh from the local farms, promoting healthy food and environment!";
+    const homeDescription2 = "Satisfaction guranteed! Our Michelin Star Chef Mike\
+    will entertain requests from customers for their food orders within reasons!";
+    const homeDescription3 = "Have no time for dine in? We do delivery, free\
+    of charges for areas within 10 km or for those with membership cards!";
+    let homeDescription = [homeDescription1, homeDescription2, homeDescription3];
     const contentContainer = document.getElementById('contentContainer');
     for(let i = 1; i <= 3; i++){
         const createModal = document.createElement('div');
         createModal.classList.add('modal');
         // Actual code for image
-        // const picture = new Image(); 
-        // picture.src = homePictureCollection[i-1];
-
-        // createModal.appendChild(picture);
-        // Mock Code for Image
-        const picture = document.createElement('div');
+        const picture = new Image(); 
+        picture.src = homePictureCollection[i-1];
         picture.classList.add('img');
-        picture.textContent = homePictureCollection[i-1];
-        
+
         createModal.appendChild(picture);
+        // Mock Code for Image
+        // const picture = document.createElement('div');
+        // picture.classList.add('img');
+        // picture.textContent = homePictureCollection[i-1];
+        
+        // createModal.appendChild(picture);
         // Mock End
         const imgDescription = document.createElement('div');
         imgDescription.classList.add('imgDescription');
-        imgDescription.textContent = 'Some text to accompany';
+        imgDescription.textContent = homeDescription[i-1];
 
         createModal.appendChild(imgDescription);
         contentContainer.appendChild(createModal);
--- src/createMenu.js
+++ src/createMenu.js
@@ -1,9 +1,25 @@
-// import bunch of picture for use later
+import menuPicture1 from './menuPicture1.png';
+import menuPicture2 from './menuPicture2.png';
+import menuPicture3 from './menuPicture3.png';
+import menuPicture4 from './menuPicture4.png';
 
 function createMenu() {
-    let menuPictureCollection = ['menuPicture1', 'menuPicture2', 'menuPicture3', 
-    'menuPicture4', 'menuPicture5', 'menuPicture6'];
-    let foodDescriptionCollection = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
+    let menuPictureCollection = [menuPicture1, menuPicture2, menuPicture3, 
+    menuPicture4, 'menuPicture5', 'menuPicture6'];
+    const foodDescription1 = "Smoked beef spaghetti carbonara! With Chef Mike's\
+    special ingredients, the carbonara is sure to excite your taste buds!";
+    const foodDescription2 = "The Rabo del Torro, a well-known stew from spain!\
+    Chef Mike's interpretation of the dish brings out the tenderness of the meat\
+    like you have never tasted before!";
+    const foodDescription3 = "Assortions of marinated sashimi, vegetables and grains\
+    ! Being healthy has never been this delicious! If you prefer non-raw fish,\
+    Chef Mike is more than happy to offer to flame torch it or switch out to another\
+    meat that is cooked!";
+    const foodDescription4 = "Chef Mike's take on Lobster Tortellini \
+    opens up your world of tastes! Blending in his hometown flavour into \
+    the cuisine, Chef Mike has imparted a proud legacy for his home"
+    let foodDescriptionCollection = [foodDescription1, foodDescription2,
+        foodDescription3, foodDescription4, 'Five', 'Six'];
     let k = 0;
 
     const contentContainer = document.getElementById('contentContainer');
@@ -23,14 +39,14 @@ function createMenu() {
             const foodDescription = document.createElement('div');
             foodDescription.textContent = foodDescriptionCollection[k];
             createFood.appendChild(foodDescription);
-            // Mock code for picture start
-            const picture = document.createElement('div')
-            picture.classList.add('right');
-            picture.textContent = menuPictureCollection[k];
-            k++;
+            // Actual code for picture
+            const picture = new Image(); 
+            picture.src = menuPictureCollection[k];
+            picture.classList.add('img');
+    
             createFood.appendChild(picture);
-            // Mock code end
             createRow.appendChild(createFood);
+            k++;
         }
         createColumnContainer.appendChild(createRow);
     }
